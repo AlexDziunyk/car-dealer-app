a9510931a26709810e8ba18ace3f04a7d55c673c
Alex Dziunyk <alexdziunyk@gmail.com>
2024-08-30 22:22:23 +0300

    Added trycatches to the fetches

--- src/app/result/[makeId]/[year]/page.tsx
+++ src/app/result/[makeId]/[year]/page.tsx
@@ -9,37 +9,57 @@ export interface ICar {
 }
 
 export async function generateStaticParams() {
-  const response = await fetch("https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
-  ).then((res) => res.json());
+  try {
+    const response = await fetch(
+      "https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json"
+    ).then((res) => res.json());
 
-  const makeIdArr = response.Results.map((item: ICar) => item.Make_ID?.toString());
+    const makeIdArr = response.Results.map((item: ICar) =>
+      item.Make_ID?.toString()
+    );
 
-  const params = makeIdArr.flatMap((makeId: string) =>
-    datesArr.map(year => ({
-      makeId,
-      year: year.toString(),
-    }))
-  );
+    const params = makeIdArr.flatMap((makeId: string) =>
+      datesArr.map((year) => ({
+        makeId,
+        year: year.toString(),
+      }))
+    );
 
-  return params;
+    return params;
+  } catch (error) {
+    console.error(error);
+    return { error: "We had a problem with finding the cars!" };
+  }
 }
 
 async function getCar(makeId: string, year: string) {
-  const car = await fetch(
-    `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`
-  ).then((res) => res.json());
+  try {
+    const car = await fetch(
+      `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`
+    ).then((res) => res.json());
 
-  return car.Results;
+    return car.Results;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
 
-const Result = async ({ params }: { params: { makeId: string; year: string } }) => {
+const Result = async ({
+  params,
+}: {
+  params: { makeId: string; year: string };
+}) => {
   const cars = await getCar(params.makeId, params.year);
 
   return (
     <div>
       <h2 className="text-lg">Year: {params.year}</h2>
       <div className="flex flex-row gap-4 justify-center aling-center mt-4">
-        {cars.map((item: ICar) => <CarItem key={item.Make_ID} car={item} />)}
+        {cars && cars.map((item: ICar) => (
+          <CarItem key={item.Make_ID} car={item} />
+        ))}
+        {!cars && <p>Oh, I'm sorry, but we couldn't find such car. Try again.</p>}
       </div>
     </div>
   );
